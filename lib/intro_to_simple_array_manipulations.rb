0808673a90bd8aba7a32b0818a772a0bbf43fb4a
Injae Lee <ilee.injae@gmail.com>
2020-04-06 04:09:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,4 +3,15 @@ def using_push(array, string)
 end
 
 def using_unshift(array, string)
-  array.
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop 
+end
+
+def pop_with_args(array)
+  array.pop(-2)
+end
+
+def 
